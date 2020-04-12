21549d8272990b47fe44ccd5e8bd713330121790
John Kurnett <john@kurnett.com>
2020-04-12 09:56:18 -0700

    Added console logs.

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ let token = '';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Loading, current token:", token)
   res.render('index', { token })
 });
 
 router.get('/auth', (req, res, next) => {
+  console.log("Setting token:", req.params.access_token)
   if (req.params.access_token) token = req.params.access_token
   res.redirect('/')
 })
