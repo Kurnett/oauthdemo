4f9e8973fc5b5cdb7ea49fbb498cf3d03373add1
John Kurnett <john@kurnett.com>
2020-04-12 09:52:16 -0700

    Added auth route.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+let token = '';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { token })
 });
+
+router.get('/auth', (req, res, next) => {
+  if (req.params.access_token) token = req.params.access_token
+  res.redirect('/')
+})
 
 module.exports = router;
